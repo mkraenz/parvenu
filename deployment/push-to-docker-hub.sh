d4863b86128965f7837af879bd2645c3ab51a8c3
Mirco Kraenz <kraenzmirco@gmail.com>
2019-05-25 20:29:13 +0200

    MOVE #25 dockerfile to top level

--- deployment/push-to-docker-hub.sh
+++ deployment/push-to-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # NOTE: env vars set by travis, or in travis repository settings
-docker build ./deployment --tag "$DOCKER_USERNAME/$DOCKER_REPO:$TRAVIS_BRANCH-$TRAVIS_JOB_NUMBER"
+docker build . --tag "$DOCKER_USERNAME/$DOCKER_REPO:$TRAVIS_BRANCH-$TRAVIS_JOB_NUMBER"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/$DOCKER_REPO:$TRAVIS_BRANCH-$TRAVIS_JOB_NUMBER
