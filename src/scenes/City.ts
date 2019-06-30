c761326f6edbc71a0e20038799a0a59ef0b89964
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-30 10:26:27 +0000

    Deploy proSingularity/parvenu to github.com/proSingularity/parvenu.git:gh-pages

--- src/scenes/City.ts
+++ src/scenes/City.ts
@@ -1,30 +1,57 @@
+import { cityConfig } from "./City.config";
 import { ICity } from "./ICity";
 import { Inventory } from "./inventory";
+import { IWareForCity } from "./IWareForCity";
 import { WareType } from "./wareType";
 
 export class City extends Inventory implements ICity {
+    constructor(wares: IWareForCity[]) {
+        super(wares);
+    }
+
+    public get(type: WareType): IWareForCity {
+        return super.get(type) as IWareForCity;
+    }
+
     /** player sells to city */
     public getSellPrice(ware: WareType, tradedQuantity: number): number {
-        const stock = this.get(ware).getQuantity();
-        if (stock < 5) {
-            return 2 * this.get(ware).price * tradedQuantity;
-        } else {
-            return this.get(ware).price * tradedQuantity;
+        const theWare = this.get(ware);
+        const currentStock = theWare.getQuantity();
+
+        let totalPrice = 0;
+        for (
+            let quantity = currentStock;
+            quantity < currentStock + tradedQuantity;
+            quantity++
+        ) {
+            totalPrice += this.getPrice(theWare, quantity);
         }
+        return Math.round(totalPrice * cityConfig.sellToBuyPriceFactor);
     }
 
     /** player buys from city */
     public getBuyPrice(ware: WareType, tradedQuantity: number): number {
-        const premium = 2;
-        const stock = this.get(ware).getQuantity();
-        if (stock <= 5) {
-            return 2 * premium * this.get(ware).price * tradedQuantity;
-        } else {
-            return premium * this.get(ware).price * tradedQuantity;
+        const theWare = this.get(ware);
+        const currentStock = theWare.getQuantity();
+        let totalPrice = 0;
+        for (
+            let quantity = currentStock;
+            quantity > currentStock - tradedQuantity;
+            quantity--
+        ) {
+            totalPrice += this.getPrice(theWare, quantity);
         }
+        return Math.round(totalPrice);
     }
 
     public hasMoney(_: number) {
         return true;
+    }
+
+    private getPrice(ware: IWareForCity, quantity: number) {
+        if (quantity === 0) {
+            return ware.maxPrice;
+        }
+        return (ware.maxPrice - ware.minPrice) / quantity + ware.minPrice;
     }
 }
---
+++ src/scenes/Ware.config.test.ts
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+import { Ware } from "./ware";
+import { WareType } from "./wareType";
+
+describe("Ware config - every WareType has set a value", () => {
+    Object.values(WareType).forEach((type: WareType) => {
+        it(`minPrice for ${type} is a positive number `, () => {
+            const ware = new Ware(type, 123);
+
+            const result = ware.minPrice;
+
+            expect(result).to.be.a("number");
+            expect(result).to.be.greaterThan(0);
+        });
+
+        it(`maxPrice for ${type} is a positive number `, () => {
+            const ware = new Ware(type, 123);
+
+            const result = ware.maxPrice;
+
+            expect(result).to.be.a("number");
+            expect(result).to.be.greaterThan(0);
+        });
+
+        it(`maxPrice for ${type} is greater than minPrice `, () => {
+            const ware = new Ware(type, 123);
+
+            const max = ware.maxPrice;
+            const min = ware.minPrice;
+
+            expect(max).to.be.greaterThan(min);
+        });
+    });
+});
