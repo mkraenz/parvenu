f5c3004062d6810add814546927484ccebc240d2
Mirco Kraenz <kraenzmirco@gmail.com>
2019-07-04 07:45:11 +0200

    CONFIG #58 Use phaser.d.ts included in phaser

--- src/game-config.ts
+++ src/game-config.ts
@@ -1,6 +1,7 @@
+import { Types } from "phaser";
 import { MainScene } from "./scenes/mainScene";
 
-export const gameConfig: GameConfig = {
+export const gameConfig: Types.Core.GameConfig = {
     height: 600,
     parent: "game",
     scene: MainScene,
--- src/game.ts
+++ src/game.ts
@@ -1,7 +1,3 @@
 import "phaser";
 
-export class Game extends Phaser.Game {
-    constructor(config: GameConfig) {
-        super(config);
-    }
-}
+export class Game extends Phaser.Game {}
