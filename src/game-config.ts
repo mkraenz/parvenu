2309e0b22646483098416da997009c7637d1f314
Mirco Kraenz <kraenzmirco@gmail.com>
2019-07-06 10:16:37 +0200

    UPDATE #44 game adjust to screen size

--- src/game-config.ts
+++ src/game-config.ts
@@ -2,9 +2,9 @@ import { Types } from "phaser";
 import { EndScene } from "./scenes/EndScene";
 import { MainScene } from "./scenes/mainScene";
 export const gameConfig: Types.Core.GameConfig = {
-    height: 600,
+    height: window.innerHeight,
+    width: window.innerWidth,
     parent: "game",
     scene: [MainScene, EndScene],
     type: Phaser.AUTO,
-    width: 800,
 };
