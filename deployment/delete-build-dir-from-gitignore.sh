a72956d981b71d99d69849d41e90ac472be9738c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-06 21:28:31 +0000

    Deploy proSingularity/parvenu to github.com/proSingularity/parvenu.git:gh-pages

--- deployment/delete-build-dir-from-gitignore.sh
+++ deployment/delete-build-dir-from-gitignore.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 echo "Deleting build directory from gitignore"
-cat .gitignore | grep -v "build" > .gitignore.without.build.tmp
-cat .gitignore.without.build.tmp > .gitignore
-rm .gitignore.without.build.tmp
+sed --expression '/build/ d' --in-place='~' ".gitignore"
