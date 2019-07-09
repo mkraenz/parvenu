5790903176eeb190d97ffbe7575addddb3e8d7ef
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-09 23:12:37 +0000

    Deploy proSingularity/parvenu to github.com/proSingularity/parvenu.git:gh-pages

--- src/Color.ts
+++ src/Color.ts
@@ -1,5 +1,5 @@
 export enum Color {
-    // TODO unify this with some method between hexadecimal and strings
+    // TODO # 84 unify this with some method between hexadecimal and strings
     Black = "#000000",
     BlackAsNumber = 0x00000,
     White = 0xffffff,
--- src/scenes/TextBuyPrice.ts
+++ src/scenes/TextBuyPrice.ts
@@ -1,21 +1,35 @@
-import { GameObjects } from "phaser";
 import { Color } from "../Color";
+import { WareType } from "../logic//WareType";
 import { ICity } from "../logic/ICity";
-import { WareType } from "../logic/WareType";
-interface ILogic {
-    city: Pick<ICity, "getBuyPrice" | "name">;
+import { ILogic as ILogicBase } from "../logic/ILogic";
+import { BaseText } from "./BaseText";
+import { KEYS } from "./keys";
+
+interface ILogic extends Pick<ILogicBase, "buy"> {
+    city: Pick<ICity, "getBuyPrice">;
 }
 
-export class TextBuyPrice extends GameObjects.Text {
+export class TextBuyPrice extends BaseText {
     private logic!: ILogic;
     private wareType!: WareType;
 
     public init(logic: ILogic, type: WareType) {
         this.logic = logic;
         this.wareType = type;
-        this.setFontFamily("Arial")
-            .setFontSize(32)
-            .setColor(Color.Black);
+
+        const graphics = this.scene.add.graphics();
+        graphics.fillStyle(Color.White, 0.7);
+        graphics.fillRoundedRect(this.x - 5, this.y - 5, 70, 50);
+        // draw on top of the graphics. Might make problems late when e.g. other panel is in front of this text,
+        this.depth = 1;
+
+        // super random values although they should be equal to fillRoundedRect()
+        const clickZone = this.scene.add.zone(this.x + 30, this.y + 20, 70, 50);
+        clickZone.setInteractive();
+        clickZone.on("pointerdown", () => {
+            this.logic.buy(this.wareType);
+            this.scene.sound.play(KEYS.sound.buy);
+        });
     }
 
     public update() {
