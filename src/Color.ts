7906bde36d81da071d503b6b74001a8ac04c8379
Mirco Kraenz <kraenzmirco@gmail.com>
2019-07-13 13:37:34 +0200

    ADD #97 Color.White

--- src/Color.ts
+++ src/Color.ts
@@ -2,5 +2,6 @@ export enum Color {
     // TODO # 84 unify this with some method between hexadecimal and strings
     Black = "#000000",
     BlackAsNumber = 0x00000,
-    White = 0xffffff,
+    WhiteAsNumber = 0xffffff,
+    White = "#ffffff",
 }
