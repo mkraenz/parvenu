a7b4b99d7ffe89d62c5dfb442e2090889a8ccc0f
Mirco Kraenz <kraenzmirco@gmail.com>
2019-07-13 19:08:15 +0200

    ADD #96 nice buttons for trade view

--- assets/images/license-notice.md
+++ assets/images/license-notice.md
@@ -1,11 +1,16 @@
 # Copyright / Attribution Notice
 
-Under CC 0
+## Under CC 0
 
 - [parchment640x480.png](https://opengameart.org/content/old-parchment-paper)
 - [background500x300.png](https://www.kisscc0.com/clipart/middle-ages-watercolor-painting-castle-castle-vree-3zn99d/)
 - [culemborg-castle800x524.png](https://www.kisscc0.com/clipart/middle-ages-watercolor-painting-castle-historic-si-insnvi/)
 
-Licensed by Matthias Möser Copyright 2018
+## Under CC BY-SA 3.0
+
+- [button-up-rect198x128](https://opengameart.org/content/user-interface-element-pack-panels-buttons-sliders-tables-icons)
+  - Author p0ss Uploaded on Wednesday, January 2, 2013 - 05:59, Changes made are filename, stretching of middle part into rectangular form
+
+## Licensed by Matthias Möser Copyright 2018
 
 - gameOver501x301.png
--- src/scenes/TextBuyPrice.ts
+++ src/scenes/TextBuyPrice.ts
@@ -1,8 +1,9 @@
-import { Color } from "../Color";
+import { GameObjects } from "phaser";
 import { WareType } from "../logic//WareType";
 import { ICity } from "../logic/ICity";
 import { ILogic as ILogicBase } from "../logic/ILogic";
 import { BaseText } from "./BaseText";
+import { getTradeButtonTweenConfig } from "./getTradeButtonTweenConfig";
 import { KEYS } from "./keys";
 
 interface ILogic extends Pick<ILogicBase, "buy"> {
@@ -12,27 +13,40 @@ interface ILogic extends Pick<ILogicBase, "buy"> {
 export class TextBuyPrice extends BaseText {
     private logic!: ILogic;
     private wareType!: WareType;
+    private button!: GameObjects.Image;
 
     public init(logic: ILogic, type: WareType) {
         this.logic = logic;
         this.wareType = type;
 
-        const graphics = this.scene.add.graphics();
-        graphics.fillStyle(Color.WhiteAsNumber, 0.7);
-        graphics.fillRoundedRect(this.x - 5, this.y - 5, 70, 50);
-        // draw on top of the graphics. Might make problems late when e.g. other panel is in front of this text,
-        this.depth = 1;
-
-        // super random values although they should be equal to fillRoundedRect()
-        const clickZone = this.scene.add.zone(this.x + 30, this.y + 20, 70, 50);
-        clickZone.setInteractive();
-        clickZone.on("pointerdown", () => {
-            this.logic.buy(this.wareType);
-            this.scene.sound.play(KEYS.sound.buy.key);
-        });
+        this.addButton();
+        this.addClickZone();
     }
 
     public update() {
         this.setText(`${this.logic.city.getBuyPrice(this.wareType, 1)}`);
+    }
+
+    private addClickZone() {
+        // super random values although they should be equal to fillRoundedRect()
+        const clickZone = this.scene.add.zone(this.x + 30, this.y + 20, 70, 50);
+        clickZone.setInteractive();
+        clickZone.on("pointerdown", () => this.onButtonClick());
+    }
+
+    private onButtonClick() {
+        // TODO only play sound / tween on successful sell
+        this.logic.buy(this.wareType);
+        this.scene.sound.play(KEYS.sound.sell.key);
+        this.scene.add.tween(getTradeButtonTweenConfig(this.button));
+    }
+
+    private addButton() {
+        const buttonCfg = KEYS.images.buttonUpRect;
+        this.button = this.scene.add
+            .image(this.x + 29, this.y + 19, buttonCfg.key)
+            .setScale(0.36);
+        // text on top of button
+        this.depth = 1;
     }
 }
