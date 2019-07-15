3f3fb6fbe08626ccf639f2e54afaecbcfdf68abe
Mirco Kraenz <kraenzmirco@gmail.com>
2019-07-15 12:27:52 +0200

    TEST #102 player and ware 100% coverage!!! yeah

--- src/logic/Ware.test.ts
+++ src/logic/Ware.test.ts
@@ -1,17 +1,29 @@
 import { expect } from "chai";
 import { uniq } from "lodash";
+import { Subject } from "rxjs";
 import { Ware } from "./Ware";
 import { WareType } from "./WareType";
 
 describe("Ware", () => {
     it("getWaresOfEachType() has correct length", () => {
         const wares = Ware.makeWaresOfEachType();
+
         expect(wares.length).to.equal(Object.keys(WareType).length);
     });
 
     it("getWaresOfEachType() has no duplicates", () => {
         const wares = Ware.makeWaresOfEachType();
+
         const uniqWareTypes = uniq(wares.map(ware => ware.type));
+
         expect(uniqWareTypes.length).to.equal(wares.length);
+    });
+
+    it("getStream()", () => {
+        const ware = new Ware(WareType.Furs, 123);
+
+        const result = ware.getStream();
+
+        expect(result).to.be.instanceOf(Subject);
     });
 });
